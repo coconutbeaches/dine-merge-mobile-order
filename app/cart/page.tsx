828abf91dc75cbda4c3fe5a664f07ebcc5393993
coconutbeaches <TypeError: Cannot read properties of undefined (reading 'options')    at getAvailableArgsOnCmd (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/lib/cmd.js:203:33)    at /Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/lib/cmd.js:164:22    at Resolved.Resolved$interpret [as _interpret] (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/node_modules/fluture/index.js:663:5)    at drain (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/node_modules/fluture/index.js:596:23)    at settle (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/node_modules/fluture/index.js:509:17)    at resolved (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/node_modules/fluture/index.js:525:7)    at EncaseN3$res (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/node_modules/fluture/index.js:1550:11)    at EncaseN$done (/Users/tyler/.nvm/versions/node/v22.17.0/lib/node_modules/gh/node_modules/fluture/index.js:1553:11)    at FSReqCallback.oncomplete (node:fs:187:23)>
2025-07-14 19:22:45 +0700

    Remove dark overlay from product images in cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -77,13 +77,12 @@ export default function Page() {
                   <CardContent className="p-3">
                     <div className="flex">
                       <div
-                        className="w-16 h-16 rounded-md mr-3 bg-center bg-cover flex-shrink-0 relative"
+                        className="w-16 h-16 rounded-md mr-3 bg-center bg-cover flex-shrink-0"
                         style={{
                           backgroundImage: `url(${item.menuItem.image ||
                             '/placeholder.svg'})`,
                         }}
                       >
-                        <div className="absolute inset-0 bg-black/10 rounded-md"></div>
                       </div>
                       <div className="flex-1 min-w-0">
                         <div className="flex justify-between items-start">
